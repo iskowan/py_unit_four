eec4818dcd3b7f6256aefb94612c70aa8843fad5
akiskowitz <iskowan@ssfs.org>
2022-10-31 15:10:32 -0400

    Commit_11

--- assignment_four.py
+++ assignment_four.py
@@ -1,3 +1,6 @@
 # Anderson Iskowitz
 #Unit 4 Option 1
 #Ace = 1
+import random
+def get_card():
+    random.randit(1, 10)
--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,21 +1,31 @@
 import random
 def who_wins(user, computer):
-    if user == computer
-        print("you tied")
-    elif user == 1 and computer == 2
-        print("You lose")
-    elif user == 2 and computer == 3
-        print ("You lose")
-    elif user == 3 and computer == 1
-        print("You lose")
-    elif user == 1 and comupter == 2
+    if user == computer:
+        print("The computer played the same as you, you tied")
+    elif user == 1 and computer == 2:
+        print("The computer played paper, you lose")
+    elif user == 1 and comupter == 3:
+        print("The computer played scissors, you lose")
+    elif user == 2 and computer == 3:
+        print ("The computer played scissors, you lose")
+    elif user == 2 and computer == 1:
+        print("The computer played rock, you win")
+    elif user == 3 and computer == 1:
+        print("The computer played rock, you Win")
+    elif user == 3 and computer == 2:
+        print("The computer played paper, you Win")
+
 # 1 is rock, 2 is paper, 3 is scissors
 
 
 def main():
+    print("This is Rock, Paper, Scissors, Rock = 1 Paper = 2 Scissors =3")
+    user = int(input("Rock, Paper, or Scissors?:"))
     computer = random.randint (1, 3)
-    user = input("Rock, Paper, or Sciccors?(1 = rock 2 = paper 3 = scissors:")
+    who_wins(user, computer)
 
+main()
 
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     main()
+'''
