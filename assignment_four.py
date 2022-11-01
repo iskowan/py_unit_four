9682763c15c034abfb863d9597f821ffcb3097c2
akiskowitz <iskowan@ssfs.org>
2022-11-01 10:11:15 -0400

    Commit_12

--- assignment_four.py
+++ assignment_four.py
@@ -4,3 +4,17 @@
 import random
 def get_card():
     random.randit(1, 10)
+get_more_cards = input("Would you like to draw another card? Yes or No?:")
+if get_more_cards == "yes" or "Yes"
+    get_card()
+elif get_more_cards -- "no" or "No"
+    print("Ok")
+def get_winner():
+    if
+def user_total()
+
+def main():
+    get_card()
+    get_card()
+
+main()
--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,26 +1,28 @@
 import random
 def who_wins(user, computer):
+    ''' 1 is rock, 2 is paper, 3 is scissors'''
     if user == computer:
         print("The computer played the same as you, you tied")
-    elif user == 1 and computer == 2:
-        print("The computer played paper, you lose")
-    elif user == 1 and comupter == 3:
-        print("The computer played scissors, you lose")
-    elif user == 2 and computer == 3:
-        print ("The computer played scissors, you lose")
-    elif user == 2 and computer == 1:
-        print("The computer played rock, you win")
-    elif user == 3 and computer == 1:
-        print("The computer played rock, you Win")
-    elif user == 3 and computer == 2:
-        print("The computer played paper, you Win")
+    elif user == "rock" or "Rock" and computer == 2:
+        print("The computer played paper, you lose!")
+    elif user == "paper" or "Paper" and comupter == 3:
+        print("The computer played scissors, you lose!")
+    elif user == "paper" or "Paper" and computer == 3:
+        print ("The computer played scissors, you lose!")
+    elif user == "paper" or "Paper" and computer == 1:
+        print("The computer played rock, you win!")
+    elif user == "Scissors" or "scissors" and computer == 1:
+        print("The computer played rock, you win!")
+    elif user == "Scissors" or "scissors" and computer == 2:
+        print("The computer played paper, you win!")
+    else:
+        print("You didnt put in rock, paper, or scissors")
 
-# 1 is rock, 2 is paper, 3 is scissors
 
 
 def main():
-    print("This is Rock, Paper, Scissors, Rock = 1 Paper = 2 Scissors =3")
-    user = int(input("Rock, Paper, or Scissors?:"))
+    print("This is Rock, Paper, Scissors")
+    user = str(input("Rock, Paper, or Scissors?:"))
     computer = random.randint (1, 3)
     who_wins(user, computer)
 
