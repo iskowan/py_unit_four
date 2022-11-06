b0a2af198b39eb3fdcb150696fb58055bae20551
akiskowitz <iskowan@ssfs.org>
2022-11-05 21:58:01 -0400

    Commit_19

--- assignment_four.py
+++ assignment_four.py
@@ -5,7 +5,6 @@ import random
 def get_card():
     return random.randint(1,10)
 
-'''
 def user_card_hand(current):
     card = get_card()
     print("You got a", card)
@@ -14,22 +13,30 @@ def user_card_hand(current):
     hand_total = card + card2
     print("Your total is", hand_total)
     return hand_total + current
-'''
 def user_total(current):
     card = get_card()
     print("You drew a", card)
     return current + card
 
+def draw_card(current):
+    card = get_card()
+    return card + current
+
 def dealer_total():
-    dealer_card1 = get_card()
-    dealer_card2 = get_card()
-    dealer_card_total = dealer_card1 + dealer_card2
-    print("Dealer total", dealer_card_total)
+    card1 = get_card()
+    card2 = get_card()
+    return card1 + card2
 
 def get_another_card():
-    input("Would you like another card?:")
-
-
+    current = 0
+    draw = input("Would you like another card?")
+    if draw == "yes":
+        card = draw_card(current)
+        print("You drew a", card)
+        #print("Your total is", user_card_hand + card)
+        get_another_card()
+    elif draw == "no":
+        print("The dealers total is...", dealer_total)
 def get_winner():
     if user_total < 21 and dealer_total < 21:
         print("You both went over 21")
@@ -42,11 +49,7 @@ def get_winner():
 
 def main():
     current = 0
-    current = user_total(current)
-    if get_another_card == "yes" or "Yes" or "Y" or "y":
-        ''' this allows the function to repeat by continuing the function if it is true/"yes" '''
-        print("Your total is", user_total(current))
-        get_another_card()
-    elif get_another_card == "no" or "No" or "N" or "n":
-        print("Your total is", current, "lets see what the dealer's total is...", dealer_total())
+    user_card_hand(current)
+    get_another_card()
+
 main()
