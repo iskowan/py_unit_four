2b793267b185fba872fd206d7889713c6b234296
akiskowitz <iskowan@ssfs.org>
2022-11-07 15:11:05 -0500

    Commit_24

--- assignment_four.py
+++ assignment_four.py
@@ -2,19 +2,16 @@
 #Unit 4 Option 1
 import random
 def get_card():
-    return random.randint(1,10)
-def user_card_hand(current):
+    return random.randint(1, 10)
+''' getting a random value between 1 and 10 '''
+def user_card_hand():
     card = get_card()
     print("You got a", card)
     card2 = get_card()
     print("You got a", card2)
     hand_total = card + card2
     print("Your total is", hand_total)
-    return hand_total + current
-def user_total(current):
-    card = get_card()
-    print("You drew a", card)
-    return current + card
+    return hand_total
 def draw_card():
     card = get_card()
     return card
@@ -22,38 +19,24 @@ def dealer_total():
     card1 = get_card()
     card2 = get_card()
     return card1 + card2
-def get_another_card():
-    current = user_card_hand
+def get_another_card(current):
     draw = input("Would you like another card?")
     if draw == "yes":
         card = draw_card()
         print("You drew a", card)
         print("Your total is", current + card)
-        '''
-        if current <= 21:
-            print("You lose")
-        '''
-        get_another_card()
+        return current + card
     elif draw == "no":
         print("The dealers total is", dealer_total)
-def get_winner(user_total, dealer_total):
-    if user_total < 21 and dealer_total < 21:
-        print("You both went over 21")
-    elif user_total == dealer_total:
-        print("You tied")
-    elif user_total > 21:
-        print("You lose")
-    elif user_total < 21 and dealer_total < user_total:
-        print("You win")
-    elif user_total < 21 and dealer_total > user_total:
-        print("You lose")
 def main():
-    current = 0
     ''' making the value of the cards 0 because you start out with no cards'''
-    user_card_hand(current)
+    current = user_card_hand()
     ''' drawing two cards and adding them to the current total'''
-    get_another_card()
+    current2 = get_another_card(current)
+    if current2 == "yes":
+        user = get_another_card(current2)
+    else:
+        print("The dealers total is", dealer_total)
     ''' asking the user if they want to draw another card, if they do, this automatically repeats '''
-    #get_winner()
-    #print(get_winner(user_total, dealer_total)
+
 main()
