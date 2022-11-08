85c144bdcd83aa7560c4c79a354c7f6dba7dbf11
akiskowitz <iskowan@ssfs.org>
2022-11-08 08:29:50 -0500

    Commit_26

--- assignment_four.py
+++ assignment_four.py
@@ -1,104 +1,9 @@
 # Anderson Iskowitz
 #Unit 4 Option 1
 import random
-'''
-def ace():
-    print(" --------")
-    print("| A      |")
-    print("|        |")
-    print("|        |")
-    print("|      A |")
-    print(" --------")
-def card2():
-    print(" --------")
-    print("| 2      |")
-    print("|        |")
-    print("|        |")
-    print("|      2 |")
-    print(" --------")
-def card3():
-    print(" --------")
-    print("| 3      |")
-    print("|        |")
-    print("|        |")
-    print("|      3 |")
-    print(" --------")
-def card4():
-    print(" --------")
-    print("| 4      |")
-    print("|        |")
-    print("|        |")
-    print("|      4 |")
-    print(" --------")
-def card5():
-    print(" --------")
-    print("| 5      |")
-    print("|        |")
-    print("|        |")
-    print("|      5 |")
-    print(" --------")
-def card6():
-    print(" --------")
-    print("| 6      |")
-    print("|        |")
-    print("|        |")
-    print("|      6 |")
-    print(" --------")
-def card7():
-    print(" --------")
-    print("| 7      |")
-    print("|        |")
-    print("|        |")
-    print("|      7 |")
-    print(" --------")
-def card8():
-    print(" --------")
-    print("| 8      |")
-    print("|        |")
-    print("|        |")
-    print("|      8 |")
-    print(" --------")
-def card9():
-    print(" --------")
-    print("| 9      |")
-    print("|        |")
-    print("|        |")
-    print("|      9 |")
-    print(" --------")
-def card10():
-    print(" --------")
-    print("| 10     |")
-    print("|        |")
-    print("|        |")
-    print("|     10 |")
-    print(" --------")
-    '''
 def get_card():
     return random.randint(1, 10)
 ''' getting a random value between 1 and 10 '''
-'''
-if get_card() == 1:
-    ace()
-if get_card() == 2:
-    card2()
-if get_card() == 3:
-    card3()
-if get_card() == 4:
-    card4()
-if get_card() == 5:
-    card5()
-if get_card() == 6:
-    card6()
-if get_card() == 7:
-    card7()
-if get_card() == 8:
-    card8()
-if get_card() == 9:
-    card9()
-if get_card() == 10:
-    card10()
-'''
-
 def user_card_hand():
     card = get_card()
     print("You got a", card)
@@ -122,15 +27,27 @@ def get_another_card(current):
         print("You drew a", card)
         total = current + card
         print("Your total is", total)
-        get_another_card(total)
         return get_another_card(total)
     elif draw == "no":
         print("The dealers total is", dealer_total)
+
+def get_winner(user, dealer):
+    if user < 21 and dealer < 21:
+        print("You both lose")
+    elif user == dealer:
+        print("You tied")
+    elif user > 21:
+        print("You lose")
+    elif user < 21 and user > dealer:
+        print("You Win")
+    elif user < 21 and user < dealer:
+        print("You lose")
+
 def main():
     ''' making the value of the cards 0 because you start out with no cards'''
     current = user_card_hand()
     ''' drawing two cards and adding them to the current total'''
-    get_another_card(current)
-    print("Your total is", get_another_card)
+    user_total = get_another_card(current)
+    get_winner(user_total, dealer_total)
 
 main()
